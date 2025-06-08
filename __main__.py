08cd80f5165e3aff1c93ee32933e5ef99bfee1a3
Othan2 <owenboyd2@gmail.com>
2025-06-08 15:51:23 -0700

    Start adding claude.

--- __main__.py
+++ __main__.py
@@ -4,11 +4,14 @@ from src.garmin.models.workout import (
     WorkoutSegment, WorkoutStep, SportType, StepType, 
     EndCondition, EndConditionType, NoTarget, PaceZoneTarget
 )
+from src.claude.client import Claude
 from getpass import getpass
 
 def main():
     email = input("Enter Garmin username: ")
     password = getpass("Enter Garmin password: ")
+    
+    
     
     with Garmin(email=email, password=password, return_on_mfa=True) as client:
         print("Connected to Garmin Connect")
